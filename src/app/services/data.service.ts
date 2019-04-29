077be4ff7df186559e28815b4d91f4917c25fb51
anurag.mundada <anurag.mundada@reisystems.com>
2019-04-29 15:04:49 -0400

    fix data service

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpResponse} from "@angular/common/http";
+import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs/index";
 
 @Injectable({
@@ -9,7 +9,7 @@ export class DataService {
 
   constructor(private http: HttpClient) { }
 
-  public getCountries(): Observable<HttpResponse> {
+  public getCountries(): Observable<any> {
     return this.http.get('https://restcountries.eu/rest/v2/all');
   }
 }
